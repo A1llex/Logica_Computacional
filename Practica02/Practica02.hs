a79988a793992585c061c4165cec06dd706876fe
Alex Fernandez <alexg.fernandeza@ciencias.unam.mx>
2020-02-24 20:10:27 -0600

    Practica 02 terminada

--- Practica02/Practica02.hs
+++ Practica02/Practica02.hs
@@ -15,34 +15,31 @@ type Indice = Int
 data LP = T | F | Var Indice| Neg LP| And LP LP | Or LP LP| Imp LP LP deriving (Eq, Show)
 
 type Evaluacion = Indice -> Bool
-
---1.2 Evalucaion
-{--
+-- 1.2 Evalucaion
+-- Funcion que evalua con evalucion
 satLP :: Evaluacion -> LP -> Bool
-satLP e phi = case phi of
- T -> True
- F -> False
- Var alpha -> (e alpha)
- Neg alpha -> not(satLP e alpha)
- And alpha beta -> (satLP e alpha) && (satLP e beta)
- Or alpha beta -> (satLP e alpha) || (satLP e beta)
- Imp alpha beta -> not(satLP e alpha) || (satPL e beta)
---}
---1.3 Modelos
-type Modelo = [Indice]
-{--
-satMod m φ= True sii m|= φ
+satLP e phi = case phi of 
+  (T) -> True
+  (F) -> False
+  (Var alpha) -> (e alpha)
+  (Neg alpha) -> not(satLP e alpha)
+  (And alpha beta) -> (satLP e alpha) && (satLP e beta)
+  (Or alpha beta) -> (satLP e alpha) || (satLP e beta)
+  (Imp alpha beta) -> not(satLP e alpha) || (satLP e beta)
 
+type Modelo = [Indice]
+-- 1.3 Modelos
+-- satMod m φ = True sii m |= φ
+--funcion que aplica ciertos valores de verdad a una formula para determinar que resulta
 satMod :: Modelo -> LP -> Bool
 satMod m phi = case phi of
- T -> True
- F->False
- Var n -> elem n m
- Oneg alpha -> not(satMod m alpha)
- Oand alpha beta -> (satMod m alpha) && (satMod m beta)
- Oor alpha beta -> (satMod m alpha) || (satMod m beta)
- Oimp alpha beta -> not(satMod m alpha) || (satMod m beta)
---}
+ (T) -> True
+ (F) -> False
+ (Var n) -> elem n m
+ (Neg alpha) -> not(satMod m alpha)
+ (And alpha beta) -> (satMod m alpha) && (satMod m beta)
+ (Or alpha beta) -> (satMod m alpha) || (satMod m beta)
+ (Imp alpha beta) -> not(satMod m alpha) || (satMod m beta)
 
 --2 Ejercicios
 
@@ -58,35 +55,35 @@ varForm (Imp a b) = nub( (varForm a ) ++ (varForm b) )
 varForm _ = []
 
 --2
---Funcion que obtendra el conjunto potencia de un conjunto
---al serconjunto se asegura que no habra elementos repetidos
-{-
-
+-- Funcion que obtendra el conjunto potencia de un conjunto
+-- al serconjunto se asegura que no habra elementos repetidos
 conjuntoPot:: [t] -> [[t]]
 conjuntoPot [] = [[]]
-conjuntoPot [a] =  [[a],[]]
-conjuntoPot (x:xs) = [[x]]++(aux x xs) ++ (conjuntoPot xs) 
--}
+conjuntoPot l = [] : (pot l)
 
-pot :: Int -> [a] -> [[a]]
-pot _ []  = [[]]
-pot 0 _   = [[]]
-pot n xs | n >= length xs = [xs]
-pot n (x:xs) = concat [map (x:) $ pot (n-1) xs, pot n xs]
-
-
-nonEmptySubsequences         :: [a] -> [[a]]
-nonEmptySubsequences []      =  []
-nonEmptySubsequences (x:xs)  =  [x] : foldr f [] (nonEmptySubsequences xs)
+--funcion auxiliar que realmente hace todo el trabajo contrullendo las combinaciones
+pot :: [a] -> [[a]]
+pot []      =  []
+pot (x:xs)  =  [x] : foldr f [] (pot xs)
   where f ys r = ys : (x : ys) : r
 
 --3
---funcion que devuelve si la formula es valida
+--funcion que devuelve si la formula es una tautologia formando todas los valores de verdad posibles de las variables
 esVal :: LP -> Bool
+esVal a 
+ | ([True] == nub (tabla (a) (conjuntoPot(varForm a))) ) = True
+ | otherwise = False
+
+--Funcion que valua con todas las combinaciones de variables geeneradas
+tabla :: LP -> [[Indice]]-> [Bool]
+tabla _ [] = []
+tabla a (x:xs)= (satMod (x) a) : (tabla a xs)
 
 --4
---funcion que devuelve si la formula es una tautologia
+--funcion que devuelve si la formula es satisfacible 
+--es decir revisa todas sus variables y genera uan tabla para indicar que todas sean T y valuar la expresion
 esSat :: LP -> Bool
+esSat  a = satMod (varForm a) a
 
 --4
 --funcion quita las implicaciones
