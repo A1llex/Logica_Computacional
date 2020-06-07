6042141cefa18737772b97e7a2fdb9a080979f7f
A1llex <alexg.fernandeza@ciencias.unam.mx>
2020-06-06 22:02:12 -0500

    Ejercico 3 añadido

--- Practicas/Practica06/Ejercicio2.pl
+++ Practicas/Practica06/Ejercicio2.pl
@@ -26,10 +26,8 @@ maullan(X,maullan):-
 %2Reglas Recursivas%
 %2.1 Naturales%
 natural(0).
-natural(X) :- 
-    X > 0,
-    X1 is X-1,
-    natural(X1).
+natural(s(X)) :- 
+    natural(X).
 
 %2.2 Fibonacci%
 fibonacci(0,1).
@@ -45,28 +43,27 @@ fibonacci(N,X) :-
 %2.3 Suma naturales%
 suma(0,X,X).
 suma(X,0,X).
-suma(X,Y,Z) :-
+/*suma(X,Y,Z) :-
     natural(X),
     natural(Y),
     X1 is X-1,
     Y1 is Y+1,
-    suma(X1,Y1,Z).
-
+    suma(X1,Y1,Z).*/
+suma(X,s(Y),Z) :-
+    suma(s(X),Y,Z).
 %2.4 potencia%
 %Multiplicacion%
 multiplicacion(1,X,X).
-multiplicacion(X,Y,Z) :-
+multiplicacion(s(X),Y,Z) :-
     natural(X),
     natural(Y),
-    X1 is X-1,
-    multiplicacion(X1, Y, Z1),
+    multiplicacion(X, Y, Z1),
     suma(Z1,Y,Z).
 %Potencia%
 potencia(_,0,1).
 potencia(X,1,X).
-potencia(X,Y,Z) :-
+potencia(X,s(Y),Z) :-
     natural(X),
     natural(Y),
-    Y1 is Y-1,
-    potencia(X,Y1,Z1),
+    potencia(X,Y,Z1),
     multiplicacion(X,Z1,Z).
---
+++ Practicas/Practica06/Ejercicio3.pl
@@ -0,0 +1,7 @@
+%Ejercicios 3 Listas%
+
+%3.1 Pertenece%
+pertenece()
+
+
+%3.2 Prefijo%
