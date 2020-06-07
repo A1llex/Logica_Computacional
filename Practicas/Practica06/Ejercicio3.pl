91ce3bc83dff4c2bc806ae9a6a2bf0cc99a76583
A1llex <alexg.fernandeza@ciencias.unam.mx>
2020-06-07 01:34:09 -0500

    Update Ejercicio3.pl

--- Practicas/Practica06/Ejercicio3.pl
+++ Practicas/Practica06/Ejercicio3.pl
@@ -1,7 +1,25 @@
 %Ejercicios 3 Listas%
 
 %3.1 Pertenece%
-pertenece()
+pertenece(X,[X|_],1).
+pertenece(X,[_|XS],N) :-
+    N1 is N-1,
+    pertenece(X,XS,N1).
 
 
 %3.2 Prefijo%
+prefijo([],_).
+prefijo([X|XS],[X|YS]) :-
+    prefijo(XS,YS).
+
+%3.3 Suma Acumulada%
+
+sumaAcumulada([],_).
+sumaAcumulada([X|XS],[]) :-
+    suml().
+
+%auxiliar% 
+suml([],_,_).
+suml([X|XS],[YS],S) :-
+    S1 is X+S,
+    suml([XS],[YS,S1],S1).
